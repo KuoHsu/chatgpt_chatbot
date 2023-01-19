2db60c20108fb6b6b1ff9ce811b4a3e79262bd27
pk9987581 <codekuo711@gmail.com>
2023-01-20 03:38:42 +0800

    'sql'

---
+++ create.sql
@@ -0,0 +1,103 @@
+-- MariaDB dump 10.19  Distrib 10.10.2-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: botdb
+-- ------------------------------------------------------
+-- Server version	10.10.2-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `group_info`
+--
+
+DROP TABLE IF EXISTS `group_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `group_info` (
+  `group_index` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` varchar(50) NOT NULL,
+  `from` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_croatian_ci DEFAULT NULL,
+  `groupname` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`group_index`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `q_type`
+--
+
+DROP TABLE IF EXISTS `q_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `q_type` (
+  `type_id` int(11) NOT NULL,
+  `description` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `qa_info`
+--
+
+DROP TABLE IF EXISTS `qa_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `qa_info` (
+  `qa_index` int(11) NOT NULL AUTO_INCREMENT,
+  `user_index` int(11) NOT NULL,
+  `group_index` int(11) DEFAULT NULL,
+  `request` varchar(1000) NOT NULL,
+  `response` varchar(1000) NOT NULL,
+  `datetime` datetime NOT NULL,
+  `type` int(11) NOT NULL,
+  PRIMARY KEY (`qa_index`),
+  KEY `FK_qa_info_group_info` (`group_index`),
+  KEY `FK_qa_info_user_info` (`user_index`),
+  KEY `qa_info_ibfk_3` (`type`),
+  CONSTRAINT `FK_qa_info_group_info` FOREIGN KEY (`group_index`) REFERENCES `group_info` (`group_index`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_qa_info_user_info` FOREIGN KEY (`user_index`) REFERENCES `user_info` (`user_index`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `qa_info_ibfk_3` FOREIGN KEY (`type`) REFERENCES `q_type` (`type_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `user_info`
+--
+
+DROP TABLE IF EXISTS `user_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_info` (
+  `user_index` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(50) NOT NULL,
+  `from` varchar(14) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  PRIMARY KEY (`user_index`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-01-20  3:33:42
